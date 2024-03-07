91fd13a7230505978247ed8b96c4d94cb1b4a4d4
Nguyễn Công Quang <98932936+Cong-Quang@users.noreply.github.com>
2024-03-07 10:21:41 +0700

    Update B1.cpp

--- Con Trỏ/B1.cpp
+++ Con Trỏ/B1.cpp
@@ -2,4 +2,10 @@
 int main(){
   int x = 0;
   int *p = &x; // gán địa chỉ x vào con trỏ p
+
+  //xin địa vùng nhớ - cấp phát
+  // void * malloc (size)
+  float * fp = (float *) malloc(10 * size of(float));
+  // calock // xoá
+  //realloc // điều kiển
 }
