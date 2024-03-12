f7291ba0295fdd736c94d61f283023624f40ceea
Cong-Quang <nguyencongquang02082003@gmail.com>
2024-03-12 14:07:53 +0700

    nothing

--- sanke.cpp
+++ sanke.cpp
@@ -14,7 +14,6 @@ void print(const char* s, int x, int y) // in ra 1 chuỗi s ở vị trí x y t
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
     printf("%s\n", s);
 }
-
 void print(const char* s, int x, int y, WORD color) // in ra 1 chuỗi s ở vị trí x y trên màn hình có màu tương ứng
 {
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
@@ -31,6 +30,9 @@ int main() {
         Sleep(1000); // 1000 milliseconds = 1 second
         print(" . ", i, i,FOREGROUND_GREEN | FOREGROUND_INTENSITY);
     }
+
+
+    
     scanf("%d");
     return 0;
 }
