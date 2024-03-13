29df0396d9aa59872017ae6cf217cf8a2dfcceeb
Cong-Quang <nguyencongquang02082003@gmail.com>
2024-03-13 09:50:27 +0700

    add cmt and random color fruit

--- snake.cpp
+++ snake.cpp
@@ -17,10 +17,10 @@ struct Point {
 
 enum Direction { UP, DOWN, LEFT, RIGHT };
 
-vector<Point> snake;
-Point fruit;
-Direction dir;
-int score;
+vector<Point> snake; // Danh sách các điểm của rắn
+Point fruit; // Vị trí của quả trái cây
+Direction dir; // Hướng di chuyển hiện tại của rắn
+int score; // Điểm số
 
 void print(const char* s, int x, int y, WORD color = 7) {
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
@@ -39,12 +39,14 @@ char readKey() {
 }
 
 void generateFruit() {
+    // Tạo vị trí mới cho quả trái cây ngẫu nhiên
     fruit.x = rand() % WIDTH;
     fruit.y = rand() % HEIGHT;
 }
 
 void update() {
     char input = readKey();
+    // Đọc phím người chơi và thiết lập hướng di chuyển
     switch (input) {
         case 'w':
             dir = UP;
@@ -61,6 +63,7 @@ void update() {
     }
 
     Point newHead = snake.front();
+    // Di chuyển đầu của rắn theo hướng đã chọn
     switch (dir) {
         case UP:
             newHead.y--;
@@ -76,11 +79,13 @@ void update() {
             break;
     }
 
+    // Kiểm tra va chạm với biên
     if (newHead.x < 0 || newHead.x >= WIDTH || newHead.y < 0 || newHead.y >= HEIGHT) {
         print("Game Over! Press any key to exit...", WIDTH / 2 - 7, HEIGHT / 2);
         readKey();
         exit(0);
     }
+    // Kiểm tra va chạm với cơ thể của rắn
     for (size_t i = 1; i < snake.size(); ++i) {
         if (newHead.x == snake[i].x && newHead.y == snake[i].y) {
             print("Game Over! Press any key to exit...", WIDTH / 2 - 7, HEIGHT / 2);
@@ -89,18 +94,19 @@ void update() {
         }
     }
 
+    // Nếu đầu của rắn chạm vào quả trái cây
     if (newHead.x == fruit.x && newHead.y == fruit.y) {
-        score++;
-        generateFruit();
+        score++; // Tăng điểm
+        generateFruit(); // Tạo quả trái cây mới
     } else {
-        snake.pop_back();
+        snake.pop_back(); // Xóa phần đuôi của rắn
     }
 
-    snake.insert(snake.begin(), newHead);
+    snake.insert(snake.begin(), newHead); // Thêm đầu mới vào đầu danh sách
 }
 
 void render() {
-    system("cls");
+    system("cls"); // Xóa màn hình
     // Vẽ đường biên
     for (int i = 0; i < WIDTH; ++i) {
         print("#", i, 0); // Đường biên trên
@@ -110,28 +116,29 @@ void render() {
         print("#", 0, i); // Đường biên trái
         print("#", WIDTH - 1, i); // Đường biên phải
     }
-    print("O", snake.front().x, snake.front().y);
+    print("O", snake.front().x, snake.front().y); // Vẽ đầu của rắn
     for (size_t i = 1; i < snake.size(); ++i) {
-        print("o", snake[i].x, snake[i].y);
+        print("o", snake[i].x, snake[i].y); // Vẽ phần thân của rắn
     }
-    print("X", fruit.x, fruit.y, 12);
+    print("P", fruit.x, fruit.y, rand() % 10 + 1); // Vẽ quả trái cây với màu sắc khác nhau
     char scoreStr[50];
     sprintf(scoreStr, "Score: %d", score);
-    print(scoreStr, 0, HEIGHT + 1);
+    print(scoreStr, 0, HEIGHT + 1); // Hiển thị điểm số
 }
 
 int main() {
     srand(time(0));
+    // Khởi tạo rắn ban đầu
     snake.push_back(Point(10, 10));
     snake.push_back(Point(9, 10));
     snake.push_back(Point(8, 10));
-    generateFruit();
-    score = 0;
-    dir = RIGHT;
+    generateFruit(); // Tạo quả trái cây ban đầu
+    score = 0; // Đặt điểm số ban đầu là 0
+    dir = RIGHT; // Hướng di chuyển ban đầu của rắn là sang phải
     while (true) {
-        update();
-        render();
-        this_thread::sleep_for(chrono::milliseconds(100));
+        update(); // Cập nhật trạng thái của trò chơi
+        render(); // Vẽ trạng thái mới của trò chơi
+        this_thread::sleep_for(chrono::milliseconds(100)); // Chờ 100ms trước khi cập nhật lại trạng thái
     }
 
     return 0;
