be3e384d7fd586518cebc57bc1bfc79c5b6fb12d
TinalNTP <tinal27725gmail.com>
2024-03-18 22:06:24 +0700

    update untitled 3

---
+++ Untitled-3.cpp
@@ -0,0 +1,28 @@
+#include <thread>
+#include <chrono>
+#include <vector>
+#include <cstdio>
+#include <conio.h>
+#include <Windows.h>
+
+void print(const char* s, int x, int y, WORD color = 7) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+    printf("%s", s);
+}
+int main() {
+    system("cls");
+   int n=13;
+      for(int i=0;i<=n;i++){
+        print("*",0,i,i);
+        print("*",n+n,i,i);
+        print("*",i+i,0,i);
+        print("*",i+i,n,i);
+        print("*",i+i,i,i);
+    } 
+    
+    return 0;
+}
